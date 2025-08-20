85e1ea86e61a476cb0f61708ff998a5933cd106e
vikash singh <146915019+vikaspal76@users.noreply.github.com>
2025-08-20 19:15:51 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -2,6 +2,12 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+# In manage.py or wsgi.py
+import os
+
+port = int(os.environ.get("PORT", 8000))
+from django.core.management import execute_from_command_line
+execute_from_command_line(["manage.py", "runserver", f"0.0.0.0:{port}"])
 
 
 def main():
